42f28ab8350c4f9638b658f7a407eb5651f2ff19
Milan Felix Šulc <rkfelix@gmail.com>
2019-05-02 08:22:54 +0200

    Feature: better package list

--- docs/.vuepress/components/AllPackages.vue
+++ docs/.vuepress/components/AllPackages.vue
@@ -6,26 +6,47 @@
     </p>
     <table class="table-packages">
       <tr>
-        <td>Name</td>
+        <td>Package</td>
+        <td>Latest</td>
         <td>Stats</td>
-        <td>Version</td>
-        <td>Description</td>
+        <td>Nette</td>
+        <td>PHP</td>
       </tr>
-      <tr v-for="(repository) of repositories" :key="repository.org + '-' + repository.name">
-        <td>
-          <a
-            :href="'/packages/'+repository.org+'/'+repository.name+'.html'"
-          >{{ repository.org }} / {{ repository.name }}</a>
-        </td>
-        <td>{{ repository.stars }} ⭐</td>
-        <td>
-          <a
-            v-if="repository.releases && repository.releases.last"
-            :href="'https://packagist.org/packages/'+repository.org+'/'+repository.name"
-          >{{repository.releases.last.tag}}</a>
-        </td>
-        <td v-html="md.render(repository.description)"></td>
-      </tr>
+      <template v-for="(repository) of repositories">
+        <tr>
+          <td class="name" rowspan="2">
+            <a
+              :href="'/packages/'+repository.org+'/'+repository.name+'.html'"
+            >{{ repository.name }} <br> <small>{{ repository.org }}</small></a>
+          </td>
+          <td>
+            <a
+              v-if="repository.releases && repository.releases.last"
+              :href="'https://packagist.org/packages/'+repository.org+'/'+repository.name"
+            >{{repository.releases.last.tag}}</a>
+          </td>
+          <td>⭐ {{ repository.stars }}</td>
+          <td>
+            <template v-if="repository.composer && repository.composer.nette">
+              <span
+                v-for="version in repository.composer.nette"
+                :key="repository.name + '-nette-'+version"
+                :class="'badge--nette--'+version.replace(/\./g,'')"
+                class="badge badge--nette"
+              >{{version}}</span>
+            </template>
+          </td>
+          <td>
+            <span
+              v-if="repository.composer && repository.composer.php"
+              class="badge badge--php"
+            >{{ repository.composer.php[0] }}-{{ repository.composer.php[1] }}</span>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="4" v-html="md.render(repository.description)"></td>
+        </tr>
+      </template>
     </table>
   </div>
 </template>
@@ -51,14 +72,15 @@ export default {
 <style lang="scss">
 .table-packages {
   tr {
-    td:nth-child(1),
-    td:nth-child(2),
-    td:nth-child(3) {
+    td.name {
       white-space: nowrap;
     }
     td > p {
       margin: 0;
     }
+  }
+  span {
+    margin: 0 1px;
   }
 }
 </style>
--- tools/sync-github.js
+++ tools/sync-github.js
@@ -1,25 +1,32 @@
 const _ = require("lodash");
 const fs = require("fs");
-
-// Global
+const path = require("path");
+const semver = require("semver");
+const LOGGER = require('debug')('github');
 const CONFIG = require('./config');
 
 (() => {
+  merge();
+  // diff();
+})();
+
+function merge() {
   const data = {};
 
   _.forEach(CONFIG.repositories, (value, key) => {
     data[key] = value;
 
-    mergeStats(data[key]);
+    mergeGithub(data[key]);
+    mergeComposer(data[key]);
   });
 
   fs.writeFileSync(
     __dirname + "/../data/repositories.json",
     JSON.stringify(data, null, 2)
   );
-})();
+}
 
-function mergeStats(repo) {
+function mergeGithub(repo) {
   const repos = CONFIG.organizations[repo.org];
   const matchedRepo = repos.find(r => r.full_name === `${repo.org}/${repo.name}`);
 
@@ -31,4 +38,104 @@ function mergeStats(repo) {
   repo.stars = matchedRepo.stargazers_count;
   repo.watchers = matchedRepo.watchers_count;
   repo.forks = matchedRepo.forks_count;
+  repo.description = matchedRepo.description;
+}
+
+function mergeComposer(repo) {
+  const repoPath = path.resolve(__dirname, '../data/vcs', repo.org, repo.name);
+
+  // Parse composer.json
+  const composer = _parseComposer(repoPath);
+  if (!composer) return;
+
+  const analyseDep = (version, name) => {
+    // Which Nette version is supported
+    if (_.startsWith(name, 'nette/')
+      || _.startsWith(name, 'tracy/')
+      || _.startsWith(name, 'latte/')
+    ) {
+      const netteVersions = [];
+      if (semver.satisfies('2.4.999', version)
+        || semver.satisfies('2.5.999', version)
+        || semver.satisfies('2.6.999', version)
+      ) {
+        netteVersions.push('2.4');
+      }
+      if (semver.satisfies('3.0.0', version)) {
+        netteVersions.push('3.0');
+      }
+      if (semver.satisfies('4.0.0', version)) {
+        netteVersions.push('4.0');
+      }
+
+      _.set(repo, 'composer.nette', netteVersions);
+    }
+
+    // Which PHP version is supported
+    if (name === 'php') {
+      const phpVersions = [];
+      const sanitizedVersion = version.replace(/\|{1}/, '||');
+      const VERSIONS = ['5.6.999', '7.0.999', '7.1.999', '7.2.999', '7.3.999', '7.4.999', '8.0.999'];
+
+      const minver = semver.minSatisfying(VERSIONS, sanitizedVersion);
+      const maxver = semver.maxSatisfying(VERSIONS, sanitizedVersion);
+
+      // if (semver.satisfies('5.6.0', version)) phpVersions.push('5.6');
+      // if (semver.satisfies('7.0.0', version)) phpVersions.push('7.0');
+      // if (semver.satisfies('7.1.0', version)) phpVersions.push('7.1');
+      // if (semver.satisfies('7.2.0', version)) phpVersions.push('7.2');
+      // if (semver.satisfies('7.3.0', version)) phpVersions.push('7.3');
+      // if (semver.satisfies('7.4.0', version)) phpVersions.push('7.4');
+      // if (semver.satisfies('8.0.0', version)) phpVersions.push('8.0');
+      // if (semver.satisfies('9.0.0', version)) phpVersions.push('9.0');
+
+      _.set(repo, 'composer.php', [
+        minver.replace(/\.999$/, ''),
+        maxver.replace(/\.999$/, ''),
+      ]);
+    }
+  }
+
+  // Deps + Devdeps
+  _.forEach(composer['require'] || [], analyseDep);
+  _.forEach(composer['require-dev'] || [], analyseDep);
+}
+
+function diff() {
+  outdated();
+  missing();
+}
+
+function outdated() {
+  _.forEach(CONFIG.repositories, (repo, repoName) => {
+    let found = false;
+    _.forEach(CONFIG.organizations, (org, orgName) => {
+      const res = _.find(CONFIG.organizations[orgName], { full_name: repoName });
+      if (res) found = true;
+    });
+
+    if (!found) {
+      console.log(`Repo ${repoName} is maybe outdated`);
+    }
+  });
+}
+
+function missing() {
+  _.forEach(CONFIG.organizations, (org, orgName) => {
+    _.forEach(org, repo => {
+      const res = _.find(CONFIG.repositories, { org: orgName, name: repo.name });
+      if (!res) {
+        console.log(`Repo ${repo.full_name} is maybe missing`);
+      }
+    });
+  });
+}
+
+function _parseComposer(path) {
+  if (!fs.existsSync(`${path}/composer.json`)) {
+    LOGGER(`File ${path}/composer.json not found`);
+    return null;
+  }
+
+  return JSON.parse(fs.readFileSync(`${path}/composer.json`));
 }

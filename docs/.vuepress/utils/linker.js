6bfe483207b223e07897b66fc477d14251157291
Milan Felix Šulc <rkfelix@gmail.com>
2019-06-20 14:26:16 +0200

    Utils: throws exception if invalid repo is passed into linker

--- docs/.vuepress/utils/linker.js
+++ docs/.vuepress/utils/linker.js
@@ -1,4 +1,6 @@
 export function link(repo) {
+  if (!repo || !repo.docs) throw "Repo argument is required";
+
   if (repo.docs.v === 'v0' || repo.docs.v === 'v1') {
     return '/packages/' + repo.org + '/' + repo.name + '.html';
   } else if (repo.docs.v === 'v2') {

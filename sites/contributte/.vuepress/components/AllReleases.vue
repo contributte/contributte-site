12cb7daeb2479b8d919848a87e047539cefbc429
Milan Felix Šulc <rkfelix@gmail.com>
2020-03-15 20:39:59 +0100

    Refactoring: improve release page

--- sites/contributte/.vuepress/components/AllReleases.vue
+++ sites/contributte/.vuepress/components/AllReleases.vue
@@ -1,32 +1,33 @@
 <template>
-  <div class="releases">
-    <div v-for="(release, id) of releases" :key="id" class="release-body mb-6">
-      <div class="text-xl">
-        <span class="text-gray-600 font-semibold">{{release.created_at|date}}</span>
-        <template v-if="$options.filters.link(release)">
-          <router-link :to="release | link">[{{release.tag}}] {{ release.org }} / {{release.repo}}</router-link>
-        </template>
-        <template v-else>
-          [{{release.tag}}] {{ release.org }} / {{release.repo}}
-        </template>
+  <div>
+    <div class="my-6">List of 20 latest releases.</div>
+    <div class="grid grid-cols-2 gap-4">
+      <div v-for="(release, id) of releases" :key="id" class="mb-6">
+        <div class="text-xl">
+          <span class="text-gray-600 font-semibold">{{release.created_at|date}}</span>
+          <template v-if="$options.filters.link(release)">
+            <router-link :to="release | link">[{{release.tag}}] {{ release.org }} / {{release.repo}}</router-link>
+          </template>
+          <template v-else>[{{release.tag}}] {{ release.org }} / {{release.repo}}</template>
+        </div>
+        <div
+          class="release-notes p-4 bg-gray-100 rounded"
+          v-html="release.body || '😢 No release notes'"
+        ></div>
       </div>
-      <div class="release-notes p-4 bg-gray-100 rounded" v-html="release.body || '😢 No release notes'"></div>
     </div>
-    <center>
-      <a v-if="releases.length < 15" @click.prevent="onLoadAll" class="bg-blue-600 hover:bg-blue-700 text-white text-2xl font-bold py-2 px-4 rounded">Load more</a>
-    </center>
   </div>
 </template>
 
 <script>
 import dayjs from "dayjs";
-import { getReleases, getFewReleases } from "./../utils/releases";
+import { getFewReleases } from "./../utils/releases";
 import { findRepository } from "./../utils/repositories";
 import { link } from "./../utils/linker";
 
 export default {
   data: () => ({
-    releases: getFewReleases(10)
+    releases: getFewReleases(20)
   }),
   filters: {
     date(s) {
@@ -39,11 +40,6 @@ export default {
       } else {
         return link(repo);
       }
-    }
-  },
-  methods: {
-    onLoadAll() {
-      this.releases = getReleases();
     }
   }
 };

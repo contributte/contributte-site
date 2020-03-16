322951ce6c60f1f40293f2b46559adcc0495fc3f
Milan Felix Šulc <rkfelix@gmail.com>
2020-03-16 20:50:32 +0100

    Releases: better style

--- sites/contributte/.vuepress/components/AllReleases.vue
+++ sites/contributte/.vuepress/components/AllReleases.vue
@@ -2,16 +2,20 @@
   <div>
     <div class="my-6">List of 20 latest releases.</div>
     <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-      <div v-for="(release, id) of releases" :key="id" class="mb-6">
-        <div class="text-xl">
-          <span class="text-gray-600 font-semibold">{{release.created_at|date}}</span>
-          <template v-if="$options.filters.link(release)">
-            <router-link :to="release | link">[{{release.tag}}] {{ release.org }} / {{release.repo}}</router-link>
-          </template>
-          <template v-else>[{{release.tag}}] {{ release.org }} / {{release.repo}}</template>
+      <div v-for="(release, id) of releases" :key="id" class="bg-white shadow-lg border rounded-lg">
+        <div class="text-lg px-4 py-2 bg-gray-100 rounded-t-lg flex flex-row">
+          <div class="flex-1">
+            <template v-if="$options.filters.link(release)">
+              <router-link
+                :to="release | link"
+              >[{{release.tag}}] {{ release.org }} / {{release.repo}}</router-link>
+            </template>
+            <template v-else>[{{release.tag}}] {{ release.org }} / {{release.repo}}</template>
+          </div>
+          <span class="font-semibold">{{release.created_at|date}}</span>
         </div>
         <div
-          class="release-notes p-4 bg-gray-100 rounded"
+          class="release-notes p-4 bg-white rounded-b-lg"
           v-html="release.body || '😢 No release notes'"
         ></div>
       </div>

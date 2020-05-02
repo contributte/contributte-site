636b30c95e7a6f6d38bcd3afc3c8a0f56d4c8a71
Milan Felix Šulc <rkfelix@gmail.com>
2020-05-02 15:26:32 +0200

    Bin: repair missing script

--- bin/misc/missing.js
+++ bin/misc/missing.js
@@ -1,9 +1,10 @@
 const _ = require("lodash");
-const CONFIG = require('./config');
 
-(() => {
-  diff();
-})();
+// Config
+const CONFIG = require('./../../contributte');
+
+const TMP_ORGS = {};
+const TMP_REPOS = {};
 
 function diff() {
   outdated();
@@ -11,10 +12,10 @@ function diff() {
 }
 
 function outdated() {
-  _.forEach(CONFIG.repositories, (repo, repoName) => {
+  _.forEach(CONFIG.resources.repositories.read(), (repo, repoName) => {
     let found = false;
-    _.forEach(CONFIG.organizations, (org, orgName) => {
-      const res = _.find(CONFIG.organizations[orgName], { full_name: repoName });
+    _.forEach(Object.keys(CONFIG.data.organizations), (org) => {
+      const res = _.find(getOrganization(org), { full_name: repoName });
       if (res) found = true;
     });
 
@@ -25,12 +26,33 @@ function outdated() {
 }
 
 function missing() {
-  _.forEach(CONFIG.organizations, (org, orgName) => {
-    _.forEach(org, repo => {
-      const res = _.find(CONFIG.repositories, { org: orgName, name: repo.name });
+  _.forEach(Object.keys(CONFIG.data.organizations), (org) => {
+    _.forEach(getOrganization(org), repo => {
+      const res = _.find(getRepositories(), { org, name: repo.name });
       if (!res) {
         console.log(`Repo ${repo.full_name} is maybe missing`);
       }
     });
   });
 }
+
+function getOrganization(org) {
+  if (!TMP_ORGS[org]) {
+    TMP_ORGS[org] = CONFIG.data.organizations[org].read();
+  }
+
+  return TMP_ORGS[org];
+}
+
+function getRepositories() {
+  if (Object.keys(TMP_REPOS).length <= 0) {
+    Object.assign(TMP_REPOS, CONFIG.resources.repositories.read());
+  }
+
+  return TMP_REPOS;
+}
+
+// @fire
+(async () => {
+  diff();
+})();

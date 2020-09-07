6a2a5448c7b5b90896f8165db6a9f5f7db7b4980
Milan Felix Šulc <rkfelix@gmail.com>
2020-09-07 15:08:51 +0200

    Sidebar: show some fixed sidebar items

--- sites/www/.vuepress/utils/data/sidebar.js
+++ sites/www/.vuepress/utils/data/sidebar.js
@@ -1,6 +1,33 @@
 import _ from "lodash";
 import { getRepositories } from './repositories';
 
+const FIXED = [
+  {
+    title: 'Get Started',
+    collapsable: false,
+    children: [
+      '/examples',
+    ]
+  },
+  {
+    title: 'Get Involved',
+    collapsable: false,
+    children: [
+      '/partners',
+      '/contributing',
+    ]
+  },
+  {
+    title: 'Development',
+    collapsable: false,
+    children: [
+      '/releases',
+      '/roadmap/challenges',
+      '/roadmap/vision',
+    ]
+  }
+];
+
 export function buildSidebar() {
   const sidebar = {};
 
@@ -9,16 +36,22 @@ export function buildSidebar() {
       const title = r.title ? r.title : _.capitalize(_.replace(r.name, /-/g, ' '));
 
       if (r.docs.v === 'v2') {
-        sidebar[`/packages/${r.org}/${r.name}/`] = [{
-          title,
-          collapsable: false,
-          path: `/packages/${r.org}/${r.name}/`,
-          children: _(r.docs.pages).map((title, path) => {
-            return [`/packages/${r.org}/${r.name}/${path}`, title];
-          }).value(),
-        }];
+        sidebar[`/packages/${r.org}/${r.name}/`] = [
+          {
+            title,
+            collapsable: false,
+            path: `/packages/${r.org}/${r.name}/`,
+            children: _(r.docs.pages).map((title, path) => {
+              return [`/packages/${r.org}/${r.name}/${path}`, title];
+            }).value(),
+          },
+          ...FIXED
+        ];
       } else {
-        sidebar[`/packages/${r.org}/${r.name}.html`] = [[`/packages/${r.org}/${r.name}.html`, title]];
+        sidebar[`/packages/${r.org}/${r.name}.html`] = [
+          [`/packages/${r.org}/${r.name}.html`, title],
+          ...FIXED
+        ];
       }
     });
 

3c72c9aea2bc7a60b6d89a1531cbd4c4ef0207cb
Milan Felix Šulc <rkfelix@gmail.com>
2020-11-07 22:50:34 +0100

    Package: improve footer

--- sites/www/.vuepress/theme/components/Metadata.vue
+++ sites/www/.vuepress/theme/components/Metadata.vue
@@ -1,12 +1,15 @@
 <template>
-  <footer class="rounded border border-gray-400 bg-gray-100 p-4 mx-8">
-    <div class="flex justify-between">
-      <div>
-        <a :href="editLink" target="_blank" rel="noopener noreferrer">Edit this page on Github!</a>
-        <OutboundLink />
-      </div>
-      <div>
-        Last update <strong>{{ lastUpdate }}</strong>
+  <footer class="footer mx-auto px-8">
+    <div class="rounded border border-gray-400 bg-gray-100 p-4">
+      <div class="md:flex justify-between">
+        <div>
+          <a :href="editLink" target="_blank" rel="noopener noreferrer">Edit this page on Github!</a>
+          <OutboundLink />
+        </div>
+        <div>
+          Last update
+          <strong>{{ lastUpdate }}</strong>
+        </div>
       </div>
     </div>
   </footer>
@@ -25,7 +28,7 @@ export default {
     },
     lastUpdate() {
       return dayjs(this.$frontmatter.git.updated).format("DD.MM.YYYY / HH:mm");
-    }
+    },
   },
 };
 </script>

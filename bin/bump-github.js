faff67dfeb8e97e09662bb06c0e84eddb62a6956
Milan Felix Šulc <rkfelix@gmail.com>
2021-08-13 13:14:31 +0200

    Www: only contributte has 2 pages

--- bin/bump-github.js
+++ bin/bump-github.js
@@ -5,12 +5,19 @@ const utils = require("./utils");
 // Config
 const CONFIG = require("./../contributte");
 
+const LEVELS = {
+  contributte: 2,
+  apitte: 1,
+  nettrine: 1,
+  ninjify: 1
+}
+
 function sync() {
   _.forEach(CONFIG.organizations, async (organization, name) => {
     console.log(`Syncing ${name} organization`);
 
     const repos = [];
-    for (const page of [1, 2]) {
+    for (const page in LEVELS[name]) {
       repos.push(...await utils.githubGet(`/orgs/${name}/repos?per_page=100&page=${page}`));
     }
     fs.writeFileSync(organization.filepath, JSON.stringify(repos, null, 2));
